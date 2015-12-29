f592cb2bc1158d4493e1ff71eee5ebe0a1840bb2
Mike <mike1072@gmail.com>
2015-12-29 03:22:54 -0800

    Version 5

---
+++ utilities/complete_packaging.bat
@@ -0,0 +1,121 @@
+@echo off
+
+REM /* make sure everything is in order */
+IF [%mod_folder%]==[] (
+  echo This batch file should not be run directly.
+  echo Please make a copy of package_real_mod.bat, edit the copy to match your mod, and run that file.
+  pause
+  exit /b
+)
+
+echo Packaging "%mod_name%, %mod_version%":
+
+REM /* set up remaining variables */
+set mod_setup=setup-%mod_folder%
+set archive_name=%mod_folder%-%mod_version%
+set win_archive=%archive_name%.exe
+set osx_archive_tar=osx-%archive_name%.tar
+set osx_archive=%osx_archive_tar%.gz
+set lin_archive_tar=lin-%archive_name%.tar
+set lin_archive=%lin_archive_tar%.gz
+set files=%mod_folder%
+
+REM /* automatically detect .tp2 located outside of mod folder */
+if exist "%mod_folder%.tp2" set files=%files% %mod_folder%.tp2
+if exist "setup-%mod_folder%.tp2" set files=%files% setup-%mod_folder%.tp2
+
+set win_files=%files% %mod_setup%.exe
+set osx_files=%files% %mod_setup% %mod_setup%.command
+set lin_files=%files%
+
+set sfx_ico=%ico_folder%\g3icon.ico
+set sfx_banner=%ico_folder%\g3banner.bmp
+set sfx_conf=mod.conf
+
+REM /* remove any previously created archives */
+del win_archive >nul 2>nul
+del osx_archive >nul 2>nul
+del lin_archive >nul 2>nul
+
+REM /* copy over latest WeiDU versions */
+copy /y "%~dp0..\weidu\weidu" %mod_setup% >nul
+copy /y "%~dp0..\weidu\weidu.exe" %mod_setup%.exe >nul
+
+REM /* copy over OS X .command script and update it to reference the current mod */
+copy /y "%~dp0..\weidu\osx.command" %mod_setup%.command >nul
+"%~dp0\sed.exe" -i.bak "s/mod_name/%mod_folder%/g" %mod_setup%.command
+del %mod_setup%.command.bak >nul 2>nul
+
+if [%lowercase_filenames%]==[1] (
+  echo   Lowercasing filenames...
+  
+  call "%~dp0\tolower.exe" %files% %mod_setup% %mod_setup%.exe %mod_setup%.command
+  
+  echo     Done.
+)
+
+REM /* list platform-exclusive files we want to exclude in other archives */
+SET sox=%audio_folder%\sox
+SET oggdec=%audio_folder%\oggdec.exe
+SET tisunpack_win32=%tispack_folder%\win32
+SET tisunpack_unix=%tispack_folder%\unix
+SET tisunpack_osx=%tispack_folder%\osx
+SET iconv=%iconv_folder%
+SET desktop_ini=%mod_folder%\desktop.ini
+SET folder_icon=%ico_folder%\g3.ico
+
+REM /* escape special characters for sed: /, &, \ */
+SET "mod_name=%mod_name:&=\&%"
+SET "mod_name=%mod_name:/=\/%"
+SET "mod_readme=%mod_readme:&=\&%"
+SET "mod_readme=%mod_readme:/=\/%"
+SET "compatible_games=%compatible_games:/=\/%"
+
+if [%build_windows%]==[1] (
+  echo   Creating %win_archive% for Windows...
+
+  REM /* duplicate the g3 template sfx configuration file and swap the actual mod information into the copy */
+  copy /y "%~dp0\g3template.conf" "%sfx_conf%" >nul
+  "%~dp0\sed.exe" -i.bak -e "s/#mod_name#/%mod_name%/g" -e "s/#mod_version#/%mod_version%/g" -e "s/#mod_readme#/%mod_readme%/g" -e "s/#mod_setup#/%mod_setup%/g" -e "s/#compatible_games#/%compatible_games%/g" %sfx_conf%
+  del %sfx_conf%.bak >nul 2>nul
+  
+  REM /* create the windows sfx archive, using our modified configuration and setting a custom icon and banner */
+  "%~dp0\winrar.exe" a -sfx -z%sfx_conf% -iicon"%sfx_ico%" -iimg"%sfx_banner%" -x%sox% -x%tisunpack_unix% -x%tisunpack_osx% %win_archive% %win_files%
+
+  REM /* remove the generated configuration file */
+  del %sfx_conf% >nul 2>nul
+
+  echo     Done.
+)
+
+if [%build_osx%]==[1] (
+  echo   Creating %osx_archive% for OS X...
+
+  REM /* create OS X archive */
+  "%~dp0\7za.exe" a -x^!%oggdec% -x^!%tisunpack_win32% -x^!%tisunpack_unix% -x^!%iconv% -x^!%desktop_ini% -x^!%folder_icon% -x^!%sfx_banner% %osx_archive_tar% %osx_files% >nul
+  "%~dp0\7za.exe" a %osx_archive% %osx_archive_tar% >nul
+  del %osx_archive_tar% >nul 2>nul
+
+  echo     Done.
+)
+
+if [%build_linux%]==[1] (
+  echo   Creating %lin_archive% for Linux...
+
+  REM /* create linux archive */
+  "%~dp0\7za.exe" a -x^!%oggdec% -x^!%sox% -x^!%tisunpack_win32% -x^!%tisunpack_osx% -x^!%iconv% -x^!%desktop_ini% -x^!%folder_icon% -x^!%sfx_banner% %lin_archive_tar% %lin_files% >nul
+  "%~dp0\7za.exe" a %lin_archive% %lin_archive_tar% >nul
+  del %lin_archive_tar% >nul 2>nul
+
+  echo     Done.
+)
+
+REM /* remove generated weidu and .command files */
+del %mod_setup%.exe >nul 2>nul
+del %mod_setup% >nul 2>nul
+del %mod_setup%.command >nul 2>nul
+
+echo   All tasks completed.
+pause
+
+endlocal
---
+++ utilities/ie_games.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM /* these variables contain HTMLized names of IE games which mods will be referenced in the SFX in the compatibility section */
+set "bg1=<acronym title=\"Baldur's Gate\">BG</acronym>"
+set "bg2=<acronym title=\"Baldur's Gate II\">BG2</acronym>"
+set "bgt=<acronym title=\"Baldur's Gate Trilogy\">BGT</acronym>"
+set "tutu=<acronym title=\"Baldur's Gate 1to2\">Tutu</acronym>"
+set "bgee=<acronym title=\"Baldur's Gate: Enhanced Edition\">BG:EE</acronym>"
+set "bgiiee=<acronym title=\"Baldur's Gate II: Enhanced Edition\">BGII:EE</acronym>"
+set "iwd1=<acronym title=\"Icewind Dale\">IWD</acronym>"
+set "iwd2=<acronym title=\"Icewind Dale II\">IWD2</acronym>"
+set "iwd-in-bg2=<acronym title=\"Icewind Dale in Baldur's Gate II\">IWD-in-BG2</acronym>"
+set "pst=<acronym title=\"Planescape: Torment\">Ps:T</acronym>"
+set "pstee=<acronym title=\"Planescape: Torment: Enhanced Edition\">Ps:T:EE</acronym>"
+set "iwdee=<acronym title=\"Icewind Dale: Enhanced Edition\">IWD:EE</acronym>"
+set "iwd2ee=<acronym title=\"Icewind Dale II: Enhanced Edition\">IWDII:EE</acronym>"
